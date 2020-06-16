1505364d276a94bb056f6f4aad4f6240a4d0c887
gemstew <newgithub@elympia.com>
2020-06-16 22:19:38 +0800

    first change

--- src/App.js
+++ src/App.js
@@ -2,23 +2,47 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+var UserNameValue;
+
+function SaveUserName(event) {
+  UserNameValue=event.target.value;
+}
+
+function PersistName() {
+  console.log(UserNameValue);
+  localStorage.setItem('UserName',UserNameValue);
+}
+
+function RetrieveName() {
+  UserNameValue = localStorage.getItem("UserName");
+}
+
+RetrieveName();
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and SAVE to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        Welcome {UserNameValue} !<br></br>
+        Enter your name
+        <br>
+        </br>
+        <input name="UserNameInput" defaultValue={UserNameValue} onChange={(event)=>SaveUserName(event)}>
+        </input>
+        <button onClick={()=>PersistName()}>
+          Save
+        </button>
+
+        <br>
+        </br>
+
+        Where were you born ?
+        <br></br>
+        
+        <input name="BirthCity" onChange={}>
+        </input>
+
+      </div>
     </div>
   );
 }
